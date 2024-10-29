f916d44c8a61e95a4de8850e4821227a3b3266ec
AliuQ <1095672504@qq.com>
2024-10-29 22:33:38 +0000

    chore: update

--- mods/config.sh
+++ mods/config.sh
@@ -122,6 +122,12 @@ install_deps() {
     run "curl -fsSL $GITHUB_RAW_URL/aliuq/run/master/files/starship.toml >~/.config/starship.toml"
     info "✔ starship 配置文件已生成"
   fi
+
+  if [ $lsb_dist = "ubuntu" ]; then
+    [ ! -f ~/.zshenv ] && run "touch ~/.zshenv"
+    run "echo \"skip_global_compinit=1\" >> ~/.zshenv"
+    info "✔ zshenv 配置已生成"
+  fi
 }
 
 # 安装 zimfw
