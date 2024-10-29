af49e814ce3b9ed5e5569b9902a9958c4faa55b7
AliuQ <1095672504@qq.com>
2024-10-29 09:56:47 +0000

    chore: update

--- mods/config.sh
+++ mods/config.sh
@@ -20,81 +20,81 @@ fi
 
 ## BEGIN
 
-install_zsh_from_ubuntu() {
-  local zsh_version=$(read_input "请输入 zsh 版本(5.9): " 5.9)
-  # local mirror_url=$(read_confirm_and_input "是否使用 mirror, 结尾要有斜杠/ (y/n): " "https://dl.llll.host/")
+# install_zsh_from_ubuntu() {
+#   local zsh_version=$(read_input "请输入 zsh 版本(5.9): " 5.9)
+#   # local mirror_url=$(read_confirm_and_input "是否使用 mirror, 结尾要有斜杠/ (y/n): " "https://dl.llll.host/")
 
-  info "==> zsh version: $(cyan $zsh_version)"
-  # info "== mirror  url: $(cyan $mirror_url)"
+#   info "==> zsh version: $(cyan $zsh_version)"
+#   # info "== mirror  url: $(cyan $mirror_url)"
 
-  if $dry_run; then run "commands_valid curl tar"; else commands_valid curl tar; fi
+#   if $dry_run; then run "commands_valid curl tar"; else commands_valid curl tar; fi
 
-  local url="https://sourceforge.net/projects/zsh/files/zsh/$zsh_version/zsh-$zsh_version.tar.xz/download"
-  echo "==> 开始解析: $url"
-  local download_url=$(curl -s "$url" | grep -oP "(?<=href=\")[^\"]+(?=\")")
-  echo "==> 解析后: $download_url"
-  sleep 1
-  download_url=$(curl -s "$download_url" | grep -oP "(?<=href=\")[^\"]+(?=\")")
-  echo "==> 解析后: $download_url"
-  sleep 1
-  local real_url="$download_url"
-  # local real_url="$mirror_url$download_url"
-  echo "==> 应用代理: $real_url"
+#   local url="https://sourceforge.net/projects/zsh/files/zsh/$zsh_version/zsh-$zsh_version.tar.xz/download"
+#   echo "==> 开始解析: $url"
+#   local download_url=$(curl -s "$url" | grep -oP "(?<=href=\")[^\"]+(?=\")")
+#   echo "==> 解析后: $download_url"
+#   sleep 1
+#   download_url=$(curl -s "$download_url" | grep -oP "(?<=href=\")[^\"]+(?=\")")
+#   echo "==> 解析后: $download_url"
+#   sleep 1
+#   local real_url="$download_url"
+#   # local real_url="$mirror_url$download_url"
+#   echo "==> 应用代理: $real_url"
 
-  run "apt install -y curl make gcc libncurses5-dev libncursesw5-dev"
-  run "curl -fsS -o /tmp/zsh.tar.xz \"$real_url\""
-  run "tar -xf /tmp/zsh.tar.xz -C /tmp"
+#   run "apt install -y curl make gcc libncurses5-dev libncursesw5-dev"
+#   run "curl -fsS -o /tmp/zsh.tar.xz \"$real_url\""
+#   run "tar -xf /tmp/zsh.tar.xz -C /tmp"
 
-  local current_dir=$(pwd)
-  run "cd /tmp/zsh-$zsh_version && ./Util/preconfig && ./configure --without-tcsetpgrp --prefix=/usr --bindir=/bin && make -j 20 install.bin install.modules install.fns"
-  run "cd $current_dir && rm -rf /tmp/zsh.tar.xz && rm -rf /tmp/zsh-$zsh_version"
-  run "zsh --version && echo \"/bin/zsh\" | tee -a /etc/shells && echo \"/usr/bin/zsh\" | tee -a /etc/shells"
-}
+#   local current_dir=$(pwd)
+#   run "cd /tmp/zsh-$zsh_version && ./Util/preconfig && ./configure --without-tcsetpgrp --prefix=/usr --bindir=/bin && make -j 20 install.bin install.modules install.fns"
+#   run "cd $current_dir && rm -rf /tmp/zsh.tar.xz && rm -rf /tmp/zsh-$zsh_version"
+#   run "zsh --version && echo \"/bin/zsh\" | tee -a /etc/shells && echo \"/usr/bin/zsh\" | tee -a /etc/shells"
+# }
 
-# 安装 zsh
-install_zsh() {
-  log "安装 zsh"
+# # 安装 zsh
+# install_zsh() {
+#   log "安装 zsh"
 
-  # if command_exists zsh; then
-  #   yellow "==> zsh 已安装, Skipping...\n"
-  #   return
-  # fi
+#   # if command_exists zsh; then
+#   #   yellow "==> zsh 已安装, Skipping...\n"
+#   #   return
+#   # fi
 
-  if $force || read_confirm "是否安装 zsh? (y/n): "; then
-    params="包管理器:推荐|源码"
-    read_from_options_show $params
-    install_type=$(read_from_options "请选择安装方式?" "1" $params true)
-    if [ $lsb_dist = "ubuntu" ]; then
-      case "$install_type" in
-      1) run "apt update -y && apt install -y zsh" ;;
-      2) install_zsh_from_ubuntu ;;
-      *)
-        red "==> 错误选项"
-        exit 0
-        ;;
-      esac
-    fi
+#   if $force || read_confirm "是否安装 zsh? (y/n): "; then
+#     params="包管理器:推荐|源码"
+#     read_from_options_show $params
+#     install_type=$(read_from_options "请选择安装方式?" "1" $params true)
+#     if [ $lsb_dist = "ubuntu" ]; then
+#       case "$install_type" in
+#       1) run "apt update -y && apt install -y zsh" ;;
+#       2) install_zsh_from_ubuntu ;;
+#       *)
+#         red "==> 错误选项"
+#         exit 0
+#         ;;
+#       esac
+#     fi
 
-    if $dry_run; then run "chsh -s $(which zsh)"; else chsh -s $(which zsh); fi
-    green "==> Default Shell: $(cyan $SHELL)"
-  fi
-}
+#     if $dry_run; then run "chsh -s $(which zsh)"; else chsh -s $(which zsh); fi
+#     green "==> Default Shell: $(cyan $SHELL)"
+#   fi
+# }
 
-# 安装 zimfw
-install_zimfw() {
-  log "安装 zimfw"
-  if echo "$BASE_URL" | grep -qE '^https?://'; then
-    is_remote=true
-  else
-    is_remote=false
-  fi
+# # 安装 zimfw
+# install_zimfw() {
+#   log "安装 zimfw"
+#   if echo "$BASE_URL" | grep -qE '^https?://'; then
+#     is_remote=true
+#   else
+#     is_remote=false
+#   fi
 
-  if $is_remote; then
-    zsh <(curl -fsSL $BASE_URL/feat/zimfw-install.sh)
-  else
-    zsh "$BASE_URL/feat/zimfw-install.sh"
-  fi
-}
+#   if $is_remote; then
+#     zsh <(curl -fsSL $BASE_URL/feat/zimfw-install.sh)
+#   else
+#     zsh "$BASE_URL/feat/zimfw-install.sh"
+#   fi
+# }
 
 # sh <(curl -sL https://raw.githubusercontent.com/aliuq/run/refs/heads/master/test/start.sh)
 # sh <(curl -sL https://raw.githubusercontent.com/aliuq/config/refs/heads/master/run.sh)
