36eb9e91c570c4b9323fff7776e69a0f3738993c
AliuQ <1095672504@qq.com>
2024-10-29 00:14:22 +0000

    chore: commit

--- test/start.sh
+++ test/start.sh
@@ -3,7 +3,8 @@
 # Usage:
 #
 
-. /workspaces/run/helper.sh
+# . /workspaces/run/helper.sh
+source <(curl -sSL https://raw.githubusercontent.com/aliuq/run/refs/heads/master/helper.sh?token=GHSAT0AAAAAACYX6PRB5XSYJ53BDHDDXW5QZZAFBYA)
 
 preset=""
 while [ $# -gt 0 ]; do
@@ -27,17 +28,6 @@ fi
 verbose=true
 
 echo_info() {
-  OS=$(uname -s)
-  KERNEL_VERSION=$(uname -r)
-  USERNAME=$(whoami)
-
-  # 检查是否在 WSL 环境中
-  if grep -qi microsoft /proc/version; then
-    IS_WSL="Yes"
-  else
-    IS_WSL="No"
-  fi
-
   # 获取完整的系统信息
   if [ -f /etc/os-release ]; then
     . /etc/os-release
@@ -52,41 +42,50 @@ echo_info() {
     OS_VERSION="Unknown"
   fi
 
-  UPTIME_RAW=$(uptime -p)
-  UPTIME_CN=$(echo "$UPTIME_RAW" | sed \
-    -e 's/up //g' \
-    -e 's/ week/周/g' \
-    -e 's/ days/天/g' \
-    -e 's/ day/天/g' \
-    -e 's/ hours/小时/g' \
-    -e 's/ hour/小时/g' \
-    -e 's/ minutes/分钟/g' \
-    -e 's/ minute/分钟/g' \
-    -e 's/,//g' \
-    -e 's/ and / /g')
+  # CPU 缓存: L1: 32 KiB / L2: 512 KiB / L3: 32 MiB
+  cpu_info=$(lscpu | awk '/L1d cache/ {L1=$3 " " $4} /L2 cache/ {L2=$3 " " $4} /L3 cache/ {L3=$3 " " $4} END {printf "L1: %s / L2: %s / L3: %s", L1, L2, L3}')
+  # 硬盘空间: 已用 / 总大小
+  disk_info=$(df -h / | awk '/\//{print $3 " / " $2}')
+  # 启动盘路径: /dev/vda1
+  boot_disk=$(df / | awk '/\//{print $1}')
+  # 内存: 已用 / 总大小
+  mem_info=$(free -h | awk '/Mem/{print $3 " / " $2}')
+  # Swap: 已用 / 总大小
+  swap_info=$(free -h | awk '/Swap/{print $3 " / " $2}')
+  # 系统在线时间: 1 day, 2 hours 3 minutes
+  uptime_info=$(uptime -p | sed 's/up //')
+  # 负载
+  loadavg=$(uptime | awk -F 'load average: ' '{print $2}' | awk '{print $1, $2, $3}')
+  # 系统: Debian GNU/Linux 10 (buster) (x86_64)
+  # 架构: x86_64
+  # 内核: 5.4.0-80-generic
+  ip_info=$(curl -sL https://myip.ipip.net/json | jq -r '.data.location | [.[0], .[1], .[2], .[3], .[4]] | @csv' | sed 's/,/ /g' | sed 's/"//g')
 
   echo
-  clear
-  printf "脚本名称    : $(white "个人开发环境管理脚本")\n"
-  printf "脚本地址    : https://github.com/aliuq/config/blob/master/run.sh\n"
-  printf "描述        : 记录一些本人经常使用的脚本操作\n"
-  printf "Shell       : %s\n" "$SHELL"
-  printf "Hostname    : %s\n" "$(hostname)"
-  printf "Username    : %s\n" "$USERNAME"
-  printf "IP          : %s\n" "$(hostname -I)"
-  printf "公网 IP     : %s\n" "$(green "$(curl -sL https://ip.llll.host)")"
-  printf "系统运行时间: %s\n" "$UPTIME_CN"
+  # clear
+  echo $(cyan "$(bold "RunShell by AliuQ")")
+  echo "-------------------------------------------------------"
+  printf "仓库地址      : %s\n" $(cyan_bright "https://github.com/aliuq/run")
+  printf "默认终端      : %s\n" $(cyan_bright "$SHELL")
+  printf "User/Host     : %s\n" $(cyan_bright "$(whoami)@$HOSTNAME")
+  printf "IP 地址       : $(hostname -I) / $(cyan_bright "$(curl -sL https://ip.llll.host)")\n"
+  printf "系统在线时间  : $(cyan_bright "$uptime_info")\n"
+  printf "连通性检查    : Github $(cyan_bright "$(check_network github)") / Google $(cyan_bright "$(check_network google)") / Cloudflare $(cyan_bright "$(check_network cloudflare)")\n"
+  printf "网络          : $(cyan_bright "$ip_info")\n"
 
   if $verbose; then
-    echo "_________________________________________\n"
-    printf "系统        : %s %s\n" "$OS_NAME" "$OS_VERSION"
-    printf "内核        : %s\n" "$KERNEL_VERSION"
-    printf "是否是 WSL  : %s\n" "$IS_WSL"
-    printf "系统架构    : %s\n" "$(uname -m)"
-    printf "Home        : %s\n" "$HOME"
-    printf "当前目录    : %s\n" "$(pwd)"
-    echo
+    echo "-------------------------------------------------------"
+    printf "CPU 缓存      : $(cyan_bright "$cpu_info")\n" 
+    printf "硬盘空间      : $(cyan_bright "$disk_info")\n" 
+    printf "启动盘路径    : $(cyan_bright "$boot_disk")\n"
+    printf "内存          : $(cyan_bright "$mem_info")\n"
+    printf "Swap          : $(cyan_bright "$swap_info")\n"
+    printf "负载          : $(cyan_bright "$loadavg")\n"
+    printf "系统          : $(cyan_bright "$OS_NAME $OS_VERSION")\n"
+    printf "架构          : $(cyan_bright "$(uname -m)")\n"
+    printf "内核          : $(cyan_bright "$(uname -r)")\n"
   fi
 }
 
 echo_info
+
