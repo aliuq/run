a4bc88b1d74366bb5b027e7ce7eeda8cd473d24e
aliuq <aliuq@bilishare.com>
2024-11-18 04:47:21 +0800

    chore: publish

--- mods/config.sh
+++ mods/config.sh
@@ -311,7 +311,7 @@ install_ohmyzsh() {
     log_success "✔ oh-my-zsh 主题修改完成 (agnoster)"
 
     # 修改插件
-    run "sed -i 's/plugins=(git)/plugins=(git starship zsh-autosuggestions zsh-syntax-highlighting zsh-completions zsh-history-substring-search eza fzf zoxide)/g' ~/.zshrc"
+    run "sed -i 's/plugins=(git)/plugins=(git starship zsh-autosuggestions zsh-syntax-highlighting zsh-completions zsh-history-substring-search eza fzf zoxide extract)/g' ~/.zshrc"
     log_success "✔ oh-my-zsh 插件修改完成"
 
     # 添加自定义配置
--- tpls/start.sh.tpl
+++ tpls/start.sh.tpl
@@ -1,6 +1,8 @@
 #!/bin/bash
 #
 # Last update: {{date}}
+# sh <(curl -sL https://raw.githubusercontent.com/aliuq/run/refs/heads/master/start.sh)
+# sh <(curl -sL https://run.xod.cc)
 
 {{helper}}
 
