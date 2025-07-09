6db2a1eca4d887c48bf8a7463cd6fa4b28a777ef
aliuq <aliuq@bilishare.com>
2025-07-09 15:13:42 +0800

    chore: release new version

--- start.sh
+++ start.sh
@@ -3,6 +3,9 @@
 # Last update: 2025-07-09
 # sh <(curl -sL https://raw.githubusercontent.com/aliuq/run/refs/heads/master/start.sh)
 # sh <(curl -sL https://run.xod.cc)
+#
+# sh <(curl -sL https://raw.llll.host/aliuq/run/refs/heads/master/start.sh)
+# sh <(curl -sL https://sh.xod.cc)
 
 # Usage:
 #
